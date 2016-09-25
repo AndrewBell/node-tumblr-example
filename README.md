30cd0486b54b6ac6b19a812a7882bbfd99bf288d
AndrewBell <andrew@recursivechaos.com>
2016-09-25 14:07:40 -0500

    Pulls credentials/blog from properties, grabs random photo url

---
+++ README.md
@@ -0,0 +1,13 @@
+# Recursive Chaos Tumblr Bot #
+
+Create a properties.ini in the /properties directory to provide your credentials. See properties-example.ini.
+
+## To run ##
+
+`npm install`
+
+`npm start`
+
+## To use ##
+
+`curl localhost:3000\random`
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var request = require('request');
+var tumblr = require('tumblr.js');
 var router = express.Router();
+var PropertiesReader = require('properties-reader');
 
-/* GET home page. */
-router.get('/', function (req, res) {
-    request('https://67.media.tumblr.com/1aa9b5c51d8441b78d15213b96f9b8d1/tumblr_odrk2alZj61to2m3po1_540.jpg').pipe(res);
+var properties = PropertiesReader('./properties/properties.ini');
+
+var client = tumblr.createClient({
+    consumer_key: properties.get('consumer_key'),
+    consumer_secret: properties.get('consumer_secret'),
+    token: properties.get('token'),
+    token_secret: properties.get('token_secret')
+});
+
+var blog = properties.get('blog');
+
+/* GET random image. */
+router.get('/random', function (req, res) {
+    console.log("Getting from: " + blog);
+    client.blogPosts(blog, {type: 'photo'}, function(err, resp) {
+        var recent = [];
+        resp.posts.forEach(function(post){
+            post.photos.forEach(function(photo){
+                console.log(photo.original_size.url);
+                recent.push(photo.original_size.url);
+            })
+        });
+        // Return actual picture
+        //request(recent[0]).pipe(res);
+        var random = Math.floor((Math.random() * recent.length));
+        console.log(random);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({url:recent[random]}));
+    });
 });
 
 module.exports = router;
